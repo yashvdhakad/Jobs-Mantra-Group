7f3ee169a67827d9001afbafaefadae5cad697ac
Yash <yashvdkd@gmail.com>
2021-07-14 19:27:40 +0530

    style: changed gradient

---
+++ js/script.js
@@ -0,0 +1,18 @@
+// console.log('hey!');
+
+
+/* let quotes = [
+    "hello world1",
+    "hello world2",
+    "hello world3"
+];
+let num = 0;
+
+function next() {
+    let x = document.getElementsByClassName('testi1');
+    num++;
+    if (num >= quotes.length) {
+        num = 0;
+    }
+    testi1.p = quotes[num];
+} */
